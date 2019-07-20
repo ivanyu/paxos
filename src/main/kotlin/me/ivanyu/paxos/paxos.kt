b785ba955f65098bd385d80f9e4ee661bdf69d64
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2019-07-20 20:21:52 +0300

    Make messages sealed classes

--- src/main/kotlin/me/ivanyu/paxos/paxos.kt
+++ src/main/kotlin/me/ivanyu/paxos/paxos.kt
@@ -6,16 +6,16 @@ typealias Value = String
 typealias ProposerId = String
 typealias AcceptorId = String
 
-interface MessageToAcceptor
-data class Prepare(val proposalId: ProposalId) : MessageToAcceptor
-data class Accept(val proposalId: ProposalId, val value: Value) : MessageToAcceptor
+sealed class MessageToAcceptor
+data class Prepare(val proposalId: ProposalId) : MessageToAcceptor()
+data class Accept(val proposalId: ProposalId, val value: Value) : MessageToAcceptor()
 
-interface MessageToProposer
+sealed class MessageToProposer
 data class Promise(val promisedProposalId: ProposalId,
                    val acceptedProposalId: ProposalId?,
-                   val acceptedValue: Value?) : MessageToProposer
+                   val acceptedValue: Value?) : MessageToProposer()
 data class Accepted(val proposalId: ProposalId,
-                    val value: Value) : MessageToProposer
+                    val value: Value) : MessageToProposer()
 
 data class ProposalId(val round: Int, val proposerId: ProposerId) : Comparable<ProposalId> {
 
