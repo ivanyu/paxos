3758b24cebe61649bd31c9ca3c38f3eb9f25d7e6
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2019-07-20 20:23:45 +0300

    Reformat tests

--- src/test/kotlin/me/ivanyu/paxos/ProposalIdTest.kt
+++ src/test/kotlin/me/ivanyu/paxos/ProposalIdTest.kt
@@ -5,19 +5,22 @@ import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class ProposalIdTest {
-    @Test fun `ProposalId should be equal to itself`() {
+    @Test
+    fun `ProposalId should be equal to itself`() {
         val b = ProposalId(1, "x")
         assertEquals(b, b)
     }
 
-    @Test fun `ProposalId should be greater if round is greater and ID is same`() {
+    @Test
+    fun `ProposalId should be greater if round is greater and ID is same`() {
         val b1 = ProposalId(1, "x")
         val b2 = ProposalId(2, "x")
         assertTrue(b2 > b1)
         assertTrue(b1 < b2)
     }
 
-    @Test fun `ProposalId should be greater if round is same and ID is greater`() {
+    @Test
+    fun `ProposalId should be greater if round is same and ID is greater`() {
         val b1 = ProposalId(1, "x")
         val b2 = ProposalId(1, "y")
         assertTrue(b2 > b1)
